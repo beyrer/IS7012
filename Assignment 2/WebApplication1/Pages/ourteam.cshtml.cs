ff4b4bfe0e69f2aec9fae6144df42db4782e216c
beyrer <beyrerjl@outlook.com>
2023-09-04 20:57:51 -0400

    updating Assignment 2

---
+++ Assignment 2/WebApplication1/Pages/ourteam.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Assignment2.Pages
+{
+    public class ourteamModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ RecruitCatBeyrerJL/Pages/ourteam.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RecruitCatBeyrerJL.Pages
+{
+    public class ourteamModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
