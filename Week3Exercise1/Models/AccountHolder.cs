7916e8ef3ec7ba61a116f95b253bc29e38de7cc4
beyrer <beyrerjl@outlook.com>
2023-09-10 16:25:15 -0400

    Week3Exercise1
    Week3Exercise1

---
+++ Week3Exercise1/Models/AccountHolder.cs
@@ -0,0 +1,14 @@
+﻿namespace Week3Exercise1.Models
+{
+    public class AccountHolder
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int Id { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public List<BankAccount> BankAccounts { get; set; }
+
+    }
+}
---
+++ Week3Exercise1/Models/BankAccount.cs
@@ -0,0 +1,15 @@
+﻿namespace Week3Exercise1.Models
+{
+    public class BankAccount
+    {
+        public string AccountNumber { get; set; }
+        public decimal CurrentBalance { get; set; }
+        public string AccountName { get; set; }
+        public int Id { get; set; }
+        public AccountHolder AccountHolder { get; set; }
+        public int AccountHolderId { get; set; }
+        public decimal PriorBalance { get; set; }
+        public string AccountType { get; set; }
+
+    }
+}
