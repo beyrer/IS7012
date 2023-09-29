543795fe0ffa0111df9a0ea57d56780db350eb1c
is7012bechmana <9tailedkruama@gmail.com>
2023-09-28 23:19:52 -0400

    Search

---
+++ BearcatSportsBuyback/Migrations/20230929030235_classupdate3.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BearcatSportsBuyback.Migrations
+{
+    public partial class classupdate3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ BearcatSportsBuyback/Pages/Members/Search.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BearcatSportsBuyback.Data;
+using BearcatSportsBuyback.Models;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace BearcatSportsBuyback.Pages.Members
+{
+    public class SearchModel : PageModel
+    {
+        private readonly BearcatSportsBuyback.Data.BearcatSportsBuybackContext _context;
+
+        public SearchModel(BearcatSportsBuyback.Data.BearcatSportsBuybackContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Member> Member { get;set; } = default!;
+        public bool SearchCompleted { get; set; }
+        public string Query { get; set; }
+
+        public async Task OnGetAsync(string query)
+        {
+            Query = query;
+            if (!string.IsNullOrWhiteSpace(query))
+            {
+                SearchCompleted = true;
+                Member = await _context.Member
+                    .Where(x => x.LastName.StartsWith(query))
+                    .ToListAsync();
+            }
+            else
+            {
+                SearchCompleted = false;
+                Member = new List<Member>();
+            }
+        }
+    }
+}
